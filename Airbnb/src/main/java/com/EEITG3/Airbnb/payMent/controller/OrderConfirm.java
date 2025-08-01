04b2bd19788371f771475ef48f2cd19aa5ba2021
Renee-Tsai <happy186565@gmail.com>
2025-08-01 12:16:29 +0800

    砍掉重練 (#44)
    controller複製人一堆全刪掉

--- Airbnb/src/main/java/com/EEITG3/Airbnb/payMent/controller/OrderConfirm.java
+++ Airbnb/src/main/java/com/EEITG3/Airbnb/payMent/controller/OrderConfirm.java
@@ -2,11 +2,13 @@ package com.EEITG3.Airbnb.payMent.controller;
 
 import java.time.temporal.ChronoUnit;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,19 +16,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.EEITG3.Airbnb.jwt.JwtService;
 import com.EEITG3.Airbnb.listing.entity.LisBean;
 import com.EEITG3.Airbnb.listing.repository.ListRepository;
+import com.EEITG3.Airbnb.payMent.dto.OrderAllResponseDto;
+import com.EEITG3.Airbnb.payMent.dto.OrderDetailResponseDto;
 import com.EEITG3.Airbnb.payMent.dto.OrderRequestDto;
 import com.EEITG3.Airbnb.payMent.entity.Order;
 import com.EEITG3.Airbnb.payMent.service.OrderService;
 import com.EEITG3.Airbnb.users.entity.Customer;
 import com.EEITG3.Airbnb.users.repository.CustomerRepository;
 
-import jakarta.servlet.http.HttpSession;
+import jakarta.transaction.Transactional;
 
 //接收 /ordersconfirm 的 POST 請求
 @RestController
 @RequestMapping("/orderconfirm")
+@Transactional
 public class OrderConfirm {
 
 	@Autowired
@@ -36,93 +42,61 @@ public class OrderConfirm {
 	private ListRepository listRepository;
 
 	@Autowired
+	private JwtService jwtService;
+
+	@Autowired
 	private CustomerRepository customerRepository;
-	
-	
 
-//@PostMapping("/confirm")
-//public ResponseEntity<?> confirmOrder(@RequestBody OrderRequestDTO dto) {
-//  // 你可以在這裡用 listId 查資料（從 ListingRepository）
-//  LisBean listing = listingRepository.findById(dto.getList_id())
-//      .orElseThrow(() -> new RuntimeException("房源不存在"));
-//
-//  // 回傳房源資訊 + DTO 給前端顯示在 orderConfirm 頁面
-//  Map<String, Object> response = new HashMap<>();
-//  response.put("listing", listing);
-//  response.put("checkin_date", dto.getCheckin_date());
-//  response.put("checkout_date", dto.getCheckout_date());
-//  response.put("people", dto.getPeople());
-//
-//  return ResponseEntity.ok(response);
-//}
+	@PostMapping("/preview")
+	public ResponseEntity<?> previewOrder(@RequestBody OrderRequestDto dto, @CookieValue(value = "jwt") String token) {
+		String email = jwtService.extractEmail(token);
+		LisBean listing = listRepository.findById(dto.getListid()).orElseThrow(() -> new RuntimeException("房源不存在"));
+		Customer customer = customerRepository.findCustomerByEmail(email)
+				.orElseThrow(() -> new RuntimeException("會員不存在"));
 
-//@PostMapping("/login")
-//public ResponseEntity<String> login(@RequestParam String customerId, HttpSession session) {
-//    session.setAttribute("customerId", customerId);
-//    return ResponseEntity.ok("登入成功！Session 建立完成");
-//}
+		long days = ChronoUnit.DAYS.between(dto.getCheckindate(), dto.getCheckoutdate());
 
+		boolean isBooked = orderService.isRoomBooked(listing.getHouseName(), dto.getCheckindate().atStartOfDay(),
+				dto.getCheckoutdate().atStartOfDay());
+		if (isBooked) {
+			return ResponseEntity.status(HttpStatus.CONFLICT).body("此日期已被預訂");
+		}
 
-//@GetMapping("/init")
-//public ResponseEntity<?> initOrderForm(@RequestParam Integer listId, HttpSession session) {
-//    String customerId = (String) session.getAttribute("customerId");
-//    if (customerId == null) {
-//        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("未登入");
-//    }
-//
-//    LisBean listing = listRepository.findById(listId)
-//        .orElseThrow(() -> new RuntimeException("找不到房源"));
-//    Customer customer = customerRepository.findById(customerId)
-//        .orElseThrow(() -> new RuntimeException("找不到會員"));
-//
-//    Map<String, Object> result = new HashMap<>();
-//    result.put("listing", listing);
-//    result.put("customer", customer);
-//
-//    return ResponseEntity.ok(result);
-//}
+		int total = listing.getPrice() * (int) days * dto.getPeople() / 2;
 
-@PostMapping("/preview")
-public ResponseEntity<?> previewOrder(@RequestBody OrderRequestDto dto) {
+		Map<String, Object> result = new HashMap<>();
+		result.put("listing", listing);
+		result.put("customer", customer);
+		result.put("days", days);
+		result.put("total", total);
+		result.put("checkindate", dto.getCheckindate());
+		result.put("checkoutdate", dto.getCheckoutdate());
+		result.put("people", dto.getPeople());
 
-    LisBean listing = listRepository.findById(dto.getListid())
-        .orElseThrow(() -> new RuntimeException("房源不存在"));
-    Customer customer = customerRepository.findById(dto.getCustomerid())
-        .orElseThrow(() -> new RuntimeException("會員不存在"));
-
-    long days = ChronoUnit.DAYS.between(dto.getCheckindate(), dto.getCheckoutdate());
-
-    boolean isBooked = orderService.isRoomBooked(
-    	    listing.getHouseName(),
-    	    dto.getCheckindate().atStartOfDay(),
-    	    dto.getCheckoutdate().atStartOfDay()
-    	);
-    if (isBooked) {
-        return ResponseEntity.status(HttpStatus.CONFLICT).body("此日期已被預訂");
-    }
-
-    int total = listing.getPrice() * (int) days * dto.getPeople() / 2;
-
-    Map<String, Object> result = new HashMap<>();
-    result.put("listing", listing);
-    result.put("customer", customer);
-    result.put("days", days);
-    result.put("total", total);
-    result.put("checkindate", dto.getCheckindate());
-    result.put("checkoutdate", dto.getCheckoutdate());
-    result.put("people", dto.getPeople());
-
-    return ResponseEntity.ok(result);
-}
-
-@PostMapping("/finalize")
-public ResponseEntity<?> finalizeOrder(@RequestBody OrderRequestDto dto) {
-    try {
-        Order order = orderService.createOrder(dto);
-        return ResponseEntity.ok("訂單成功！訂單編號：" + order.getBookingid());
-    } catch (Exception e) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body("建立訂單失敗：" + e.getMessage());
-    	}
+		return ResponseEntity.ok(result);
 	}
+
+	@PostMapping("/finalize")
+	public ResponseEntity<?> finalizeOrder(@RequestBody OrderRequestDto dto, @CookieValue(value = "jwt") String token) {
+		try {
+			String email = jwtService.extractEmail(token);
+			Order order = orderService.createOrder(dto,email);
+			return ResponseEntity.ok("訂單成功！訂單編號：" + order.getBookingid());
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("建立訂單失敗：" + e.getMessage());
+		}
+	}
+	//客戶查詢全部訂單
+	@GetMapping("/detail")
+	public OrderDetailResponseDto getOrderDetail(@RequestParam("booking_id") String booking_id) {
+	return orderService.getOrderByBookingId(booking_id);
+	    }
+	
+	//單筆訂單明細
+    @GetMapping("/byCustomer")
+    public List<OrderAllResponseDto> getOrdersByCustomerId(@CookieValue(value = "jwt") String token) {
+        String email = jwtService.extractEmail(token);
+        
+    	return orderService.getOrdersByCustomerId(email);
+    }	
 }
