6ab3388313d5f6af65bf7b5837ff6134e12eea15
Wu <eszrdx417@gmail.com>
2025-08-01 11:04:08 +0800

    8/1 VUE 環境設定

--- front-end/src/main.ts
+++ front-end/src/main.ts
@@ -1,14 +1,34 @@
-import './assets/main.css'
+//import './assets/main.css'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 
 import App from './App.vue'
-import router from './router'
-
 const app = createApp(App)
 
-app.use(createPinia())
+//router
+import router from './router'
 app.use(router)
+
+
+// vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
+ // Ensure you are using css-loader
+
+const vuetify = createVuetify({
+  
+  components,
+  directives,
+})
+
+app.use(vuetify)
+
+//pinia
+import { createPinia } from 'pinia'
+app.use(createPinia())
+
 
 app.mount('#app')
