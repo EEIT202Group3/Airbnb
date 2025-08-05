fce057b2042e1e69ba07be59ce0d53806dce2717
NickChen527 <nickck0527@gmail.com>
2025-08-05 18:47:26 +0800

    8/5PM
    1.管理者前端網頁建置

---
+++ front-end/src/components/users/CustomerList.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { ref,onMounted } from 'vue';
+import { getAllCustomers } from '@/service/user/CustomerService';
+const customers = ref([]);
+onMounted(async()=>{customers.value = await getAllCustomers();});
+</script>
+<template>
+    <h1>客戶清單</h1>
+    <v-table v-if="!(customers===null)">
+        <thead>
+            <tr>
+                <th>email</th>
+                <th>username</th>
+                <th>createAt</th>
+                <th>isActive</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="customer in customers">
+                <td>{{ customer.email }}</td>
+                <td>{{ customer.username }}</td>
+                <td>{{ customer.createAt }}</td>
+                <td>{{ customer.isActive }}</td>
+            </tr>
+        </tbody>
+    </v-table>
+</template>
+<style scoped>
+</style>
