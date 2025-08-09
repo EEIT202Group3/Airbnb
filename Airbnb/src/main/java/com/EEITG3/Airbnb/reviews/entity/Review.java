ac086074ddec5b0bb9e852b1b04cd80d3b0f4117
Wu666 <103838405+Wus77@users.noreply.github.com>
2025-08-09 16:11:27 +0800

    8/9 VUE 評論前台修改

--- Airbnb/src/main/java/com/EEITG3/Airbnb/reviews/entity/Review.java
+++ Airbnb/src/main/java/com/EEITG3/Airbnb/reviews/entity/Review.java
@@ -2,11 +2,16 @@ package com.EEITG3.Airbnb.reviews.entity;
 
 import org.springframework.stereotype.Component;
 
+import com.EEITG3.Airbnb.listing.entity.LisBean;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -23,7 +28,6 @@ import lombok.RequiredArgsConstructor;
 @AllArgsConstructor
 @RequiredArgsConstructor
 public class Review {
-	
 
 	@Id @Column(name = "review_id")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
---
+++ ctwify/src/components/reviews/ReviewView.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-row>
+    <ReviewCard
+      v-for="r in reviews"
+      :index="r.id"
+      :review="r"
+      justify="center"
+    ></ReviewCard>
+  </v-row>
+</template>
+
+<script setup>
+import api from "@/api";
+import ReviewCard from "@/components/reviews/customer/ReviewCard.vue";
+import { ref, onMounted } from "vue";
+const reviews = ref({});
+const list = ref({});
+
+onMounted(async () => {
+  reviews.value = (await api.get("/api/reviews")).data;
+  console.log(reviews.value);
+  list.value = (await api.get("/listings/simple")).data;
+  console.log(list.value);
+});
+</script>
+
+<style></style>
