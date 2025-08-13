f3afdf8723cf04718bb4f19cec3106255b448587
NickChen527 <nickck0527@gmail.com>
2025-08-13 16:14:01 +0800

    8/13PM
    改樣式

--- ctwify/src/components/user/host/HostPage.vue
+++ ctwify/src/components/user/host/HostPage.vue
@@ -3,14 +3,40 @@ import HostSideBar from './HostSideBar.vue';
 </script>
 
 <template>
-    <v-container style="margin-top: 50px;">
-        <v-row>
-        <v-col cols="3" style="margin-top: 15px;">
+    <v-container class="customer-page">
+        <v-row class="d-flex justify-center">
+        <v-col cols="3" style="margin-top: 40px;">
             <HostSideBar></HostSideBar>
         </v-col>
-        <v-col cols="9">
+        <v-col cols="7" style="margin-top: 15px;">
             <router-view></router-view>
         </v-col>
     </v-row>
     </v-container>
 </template>
+<style scoped>
+.customer-page {
+  position: relative; /* 要讓 ::before 定位基準 */
+  min-height: 88vh;
+  min-width: 100%;
+  background-image: url('@/images/hostbackground.jpg');
+  background-size: cover;
+  background-position: center;
+  background-attachment: fixed;
+}
+
+/* 半透明覆蓋層 */
+.customer-page::before {
+  content: "";
+  position: absolute;
+  inset: 0; /* 等同 top:0; right:0; bottom:0; left:0; */
+  background-color: rgba(255, 255, 255, 0.4); /* 白色+透明度，越高越霧 */
+  z-index: 0;
+}
+
+/* 內容要在遮罩上面 */
+.customer-page > * {
+  position: relative;
+  z-index: 1;
+}
+</style>
